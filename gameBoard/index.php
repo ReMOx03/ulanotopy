ffc07a03b413cac34a85aaaee335d0448b489d98
ReMOx03 <62967414+ReMOx03@users.noreply.github.com>
2020-12-08 19:48:58 +0100

    Add files via upload

---
+++ gameBoard/index.php
@@ -0,0 +1,678 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Prototyp planszy</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+</head>
+<body>
+	<div id="gamePlaceHolder">
+		<div class="players">
+			<div id="player1">
+				<div class="playerIn">
+					<div class="playerName">
+						
+					</div>
+					<div class="money">
+						Pieniądze: <br>
+						<div class="moneyAmmount">
+							<div class="moneyNumber">0</div>
+							<div>$</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="player2">
+				<div class="playerIn">
+					<div class="playerName">
+					</div>
+					<div class="money">
+						Pieniądze: <br>
+						<div class="moneyAmmount">
+							<div class="moneyNumber">0</div>
+							<div>$</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="gameBoard">
+			<div id="gamebordBack">
+			<div class="gameClusters">
+				<div class="cornerBlock">
+					<div class="cityBlock">
+						<div class="cityName">
+							<div id="N17">MISTRZOSTWA<br> ŚWIATA</div>
+						</div>
+						<div class="playersInCity">
+							<div id="playerInCity17">
+								<br>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="normalBlock colorOfThisBlock">
+					<div class="cityBlock">
+						<div class="cityLvl">
+							<div class="lvlN">LVL: </div>
+							<div id="L18">0</div>
+						</div>
+						<div class="cityName">
+							<div id="N18">LONDYN</div>
+						</div>
+						<div class="playersInCity">
+							<div id="playerInCity18">
+								<br>
+							</div>
+						</div>
+						<div class="rentalCost">
+							Koszt:
+							<div id="C18">0</div>
+						</div>
+					</div>
+				</div>
+				<div class="normalBlock cityBlockISLA">
+					<div>
+						<div class="cityLvl">
+							<div class="lvlN">LVL: </div>
+							<div id="L19">0</div>
+						</div>
+						<div class="cityName">
+							<div id="N19">DUBAJ</div>
+						</div>
+						<div class="playersInCity">
+							<div id="playerInCity19">
+								<br>
+							</div>
+						</div>
+						<div class="rentalCost">
+							Koszt:
+							<div id="C19">0</div>
+						</div>
+					</div>
+				</div>
+				<div class="normalBlock colorOfThisBlock">
+					<div class="cityBlock">
+						<div class="cityLvl">
+							<div class="lvlN">LVL: </div>
+							<div id="L20">0</div>
+						</div>
+						<div class="cityName">
+							<div id="N20">SYDNEY</div>
+						</div>
+						<div class="playersInCity">
+							<div id="playerInCity20">
+								<br>
+							</div>
+						</div>
+						<div class="rentalCost">
+							Koszt:
+							<div id="C20">0</div>
+						</div>
+					</div>
+				</div>
+				<div class="normalBlock chance">
+					<div>SZANSA</div>
+					<div class="playersInCity">
+						<div id="playerInCity11">
+							
+						</div>
+					</div>
+				</div>
+				<div class="normalBlock colorOfThisBlock">
+					<div class="cityBlock">
+						<div class="cityLvl">
+							<div class="lvlN">LVL: </div>
+							<div id="L22">0</div>
+						</div>
+						<div class="cityName">
+							<div id="N22">CHICAGO</div>
+						</div>
+						<div class="playersInCity">
+							<div id="playerInCity22">
+								<br>
+							</div>
+						</div>
+						<div class="rentalCost">
+							Koszt:
+							<div id="C22">0</div>
+						</div>
+					</div>
+				</div>
+				<div class="normalBlock colorOfThisBlock">
+					<div class="cityBlock">
+						<div class="cityLvl">
+							<div class="lvlN">LVL: </div>
+							<div id="L23">0</div>
+						</div>
+						<div class="cityName">
+							<div id="N23">VEGAS</div>
+						</div>
+						<div class="playersInCity">
+							<div id="playerInCity23">
+								<br>
+							</div>
+						</div>
+						<div class="rentalCost">
+							Koszt:
+							<div id="C23">0</div>
+						</div>
+					</div>
+				</div>
+				<div class="normalBlock colorOfThisBlock">
+					<div class="cityBlock">
+						<div class="cityLvl">
+							<div class="lvlN">LVL: </div>
+							<div id="L24">0</div>
+						</div>
+						<div class="cityName">
+							<div id="N24">JORK</div>
+						</div>
+						<div class="playersInCity">
+							<div id="playerInCity24">
+								<br>
+							</div>
+						</div>
+						<div class="rentalCost">
+							Koszt:
+							<div id="C24">0</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="cornerBlock">
+					<div class="cityBlock">
+						<div class="cityName">
+							<div id="N25">PODRÓŻ</div>
+						</div>
+					</div>
+					<div class="playersInCity">
+						<div id="playerInCity25">
+							<br>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+
+			<div class="gameClusters">
+				<div class="normalBlockM colorOfThisBlock">
+					<div class="cityBlock">
+						<div class="cityLvlBlock">
+							<div class="cityLvl">
+								<div class="lvlN">LVL:</div>
+								<div id="L16">0</div>
+							</div>
+							<div class="cityName">
+								<div id="N16">MEDIOLAN</div>
+							</div>
+							<div class="playersInCity">
+								<div id="playerInCity16">
+									
+								</div>
+							</div>
+						</div>
+						<div class="rentalCostM rentalCost">
+							Koszt:
+							<div id="C16">0</div>
+						</div>
+					</div>
+				</div>
+			<div class="normalBlockM  cityBlockISLA right">
+					<div class="cityBlock">
+						<div class="cityLvlBlock">
+							<div class="cityLvl">
+								<div class="lvlN">LVL:</div>
+								<div id="L26">0</div>
+							</div>
+							<div class="cityName">
+								<div id="N26">CYPR</div>
+							</div>
+							<div class="playersInCity">
+								<div id="playerInCity26">
+									
+								</div>
+							</div>
+						</div>
+						<div class="rentalCostM rentalCost">
+							Koszt:
+							<div id="C26">0</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="gameClusters">
+				<div class="normalBlockM  cityBlockISLA">
+					<div class="cityBlock">
+						<div class="cityLvlBlock">
+							<div class="cityLvl">
+								<div class="lvlN">LVL:</div>
+								<div id="L15">0</div>
+							</div>
+							<div class="cityName">
+								<div id="N15">CYPR</div>
+							</div>
+							<div class="playersInCity">
+								<div id="playerInCity15">
+									
+								</div>
+							</div>
+						</div>
+						<div class="rentalCostM rentalCost">
+							Koszt:
+							<div id="C15">0</div>
+						</div>
+					</div>
+				</div>
+				<div class="normalBlockM right colorOfThisBlock">
+					<div class="cityBlock">
+						<div class="cityLvlBlock">
+							<div class="cityLvl">
+								<div class="lvlN">LVL:</div>
+								<div id="L27">0</div>
+							</div>
+							<div class="cityName">
+								<div id="N27">LYON</div>
+							</div>
+							<div class="playersInCity">
+								<div id="playerInCity27">
+									
+								</div>
+							</div>
+						</div>
+						<div class="rentalCostM rentalCost">
+							Koszt:
+							<div id="C27">0</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="gameClusters">
+				<div class="normalBlockM colorOfThisBlock">
+					<div class="cityBlock">
+						<div class="cityLvlBlock">
+							<div class="cityLvl">
+								<div class="lvlN">LVL:</div>
+								<div id="L14">0</div>
+							</div>
+							<div class="cityName">
+								<div id="N14">MEDIOLAN</div>
+							</div>
+							<div class="playersInCity">
+								<div id="playerInCity14">
+									
+								</div>
+							</div>
+						</div>
+						<div class="rentalCostM rentalCost">
+							Koszt:
+							<div id="C14">0</div>
+						</div>
+					</div>
+				</div>
+				<div class="normalBlockM right colorOfThisBlock">
+					<div class="cityBlock">
+						<div class="cityLvlBlock">
+							<div class="cityLvl">
+								<div class="lvlN">LVL:</div>
+								<div id="L28">0</div>
+							</div>
+							<div class="cityName">
+								<div id="N28">PARYŻ</div>
+							</div>
+							<div class="playersInCity">
+								<div id="playerInCity28">
+									
+								</div>
+							</div>
+						</div>
+						<div class="rentalCostM rentalCost">
+							Koszt:
+							<div id="C28">0</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="gameClusters">
+				<div class="normalBlockM chance">
+					<div>SZANSA</div>
+					<div class="playersInCity">
+						<div id="playerInCity11">
+							
+						</div>
+					</div>
+				</div>
+				<div class="normalBlockM right chance">
+					<div>SZANSA</div>
+					<div class="playersInCity">
+						<div id="playerInCity29">
+							
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="gameClusters">
+				<div class="normalBlockM colorOfThisBlock">
+					<div class="cityBlock">
+						<div class="cityLvlBlock">
+							<div class="cityLvl">
+								<div class="lvlN">LVL:</div>
+								<div id="L11">0</div>
+							</div>
+							<div class="cityName">
+								<div id="N11">RZYM</div>
+							</div>
+							<div class="playersInCity">
+								<div id="playerInCity11">
+									
+								</div>
+							</div>
+						</div>
+						<div class="rentalCostM rentalCost">
+							Koszt:
+							<div id="C11">0</div>
+						</div>
+					</div>
+				</div>
+				<div class="normalBlockM right colorOfThisBlock">
+					<div class="cityBlock">
+						<div class="cityLvlBlock">
+							<div class="cityLvl">
+								<div class="lvlN">LVL:</div>
+								<div id="L30">0</div>
+							</div>
+							<div class="cityName">
+								<div id="N30">KRAKÓW</div>
+							</div>
+							<div class="playersInCity">
+								<div id="playerInCity30">
+									
+								</div>
+							</div>
+						</div>
+						<div class="rentalCostM rentalCost">
+							Koszt:
+							<div id="C30">0</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="gameClusters">
+				<div class="normalBlockM colorOfThisBlock">
+					<div class="cityBlock">
+						<div class="cityLvlBlock">
+							<div class="cityLvl">
+								<div class="lvlN">LVL:</div>
+								<div id="L10">0</div>
+							</div>
+							<div class="cityName">
+								<div id="N10">MEDIOLAN</div>
+							</div>
+							<div class="playersInCity">
+								<div id="playerInCity10">
+									
+								</div>
+							</div>
+						</div>
+						<div class="rentalCostM rentalCost">
+							Koszt:
+							<div id="C10">0</div>
+						</div>
+					</div>
+				</div>
+				<div class="normalBlockM right chance">
+					<div>PODATEK</div>
+					<div class="playersInCity">
+						<div id="playerInCity31">
+							
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="gameClusters">
+				<div class="normalBlockM colorOfThisBlock">
+					<div class="cityBlock">
+						<div class="cityLvlBlock">
+							<div class="cityLvl">
+								<div class="lvlN">LVL:</div>
+								<div id="L9">0</div>
+							</div>
+							<div class="cityName">
+								<div id="N9">WENECJA</div>
+							</div>
+							<div class="playersInCity">
+								<div id="playerInCity9">
+									
+								</div>
+							</div>
+						</div>
+						<div class="rentalCostM rentalCost">
+							Koszt:
+							<div id="C9">0</div>
+						</div>
+					</div>
+				</div>
+				<div class="normalBlockM right colorOfThisBlock">
+					<div class="cityBlock">
+						<div class="cityLvlBlock">
+							<div class="cityLvl">
+								<div class="lvlN">LVL:</div>
+								<div id="L32">0</div>
+							</div>
+							<div class="cityName">
+								<div id="N32">WARSZAWA</div>
+							</div>
+							<div class="playersInCity">
+								<div id="playerInCity32">
+									
+								</div>
+							</div>
+						</div>
+						<div class="rentalCostM rentalCost">
+							Koszt:
+							<div id="C32">0</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+
+
+			<div class="gameClusters">
+				<div class="cornerBlock">
+					<div id="startBlock">
+						BEZ<br>LUDZI<br>WYSPA
+					</div>
+					<div class="playersInCity">
+						<div id="playerInCity8">
+							<br>
+						</div>
+					</div>
+				</div>
+				<div class="normalBlock colorOfThisBlock">
+					<div class="cityBlock">
+						<div class="cityLvl">
+							<div class="lvlN">LVL: </div>
+							<div id="L7">0</div>
+						</div>
+						<div class="cityName">
+							<div id="N7">SHANGHAI</div>
+						</div>
+						<div class="playersInCity">
+							<div id="playerInCity7">
+								<br>
+							</div>
+						</div>
+						<div class="rentalCost">
+							Koszt:
+							<div id="C7">0</div>
+						</div>
+					</div>
+				</div>
+				<div class="normalBlock colorOfThisBlock">
+					<div class="cityBlock">
+						<div class="cityLvl">
+							<div class="lvlN">LVL: </div>
+							<div id="L6">0</div>
+						</div>
+						<div class="cityName">
+							<div id="N6">PEKIN</div>
+						</div>
+						<div class="playersInCity">
+							<div id="playerInCity6">
+								<br>
+							</div>
+						</div>
+						<div class="rentalCost">
+							Koszt:
+							<div id="C6">0</div>
+						</div>
+					</div>
+				</div>
+				<div class="normalBlock colorOfThisBlock">
+					<div class="cityBlock">
+						<div class="cityLvl">
+							<div class="lvlN">LVL: </div>
+							<div id="L5">0</div>
+						</div>
+						<div class="cityName">
+							<div id="N5">HONGKONG</div>
+						</div>
+						<div class="playersInCity">
+							<div id="playerInCity5">
+								<br>
+							</div>
+						</div>
+						<div class="rentalCost">
+							Koszt:
+							<div id="C5">0</div>
+						</div>
+					</div>
+				</div>
+				<div class="normalBlock cityBlockISLA">
+					<div>
+						<div class="cityLvl">
+							<div class="lvlN">LVL: </div>
+							<div id="L4">0</div>
+						</div>
+						<div class="cityName">
+							<div id="N4">BALI</div>
+						</div>
+						<div class="playersInCity">
+							<div id="playerInCity4">
+								<br>
+							</div>
+						</div>
+						<div class="rentalCost">
+							Koszt:
+							<div id="C4">0</div>
+						</div>
+					</div>
+				</div>
+				<div class="normalBlock colorOfThisBlock">
+					<div class="cityBlock">
+						<div class="cityLvl">
+							<div class="lvlN">LVL: </div>
+							<div id="L3">0</div>
+						</div>
+						<div class="cityName">
+							<div id="N3">MADRID</div>
+						</div>
+						<div class="playersInCity">
+							<div id="playerInCity3">
+								<br>
+							</div>
+						</div>
+						<div class="rentalCost">
+							Koszt:
+							<div id="C3">0</div>
+						</div>
+					</div>
+				</div>
+				<div class="normalBlock colorOfThisBlock">
+					<div class="cityBlock">
+						<div class="cityLvl">
+							<div class="lvlN">LVL: </div>
+							<div id="L2">0</div>
+						</div>
+						<div class="cityName">
+							<div id="N2">SEVILLA</div>
+						</div>
+						<div class="playersInCity">
+							<div id="playerInCity2">
+								<br>
+							</div>
+						</div>
+						<div class="rentalCost">
+							Koszt:
+							<div id="C2">0</div>
+						</div>
+					</div>
+				</div>
+				<div class="normalBlock colorOfThisBlock">
+					<div class="cityBlock">
+						<div class="cityLvl">
+							<div class="lvlN">LVL: </div>
+							<div id="L1">0</div>
+						</div>
+						<div class="cityName">
+							<div id="N1">GRANADA</div>
+						</div>
+						<div class="playersInCity">
+							<div id="playerInCity1">
+								<br>
+							</div>
+						</div>
+						<div class="rentalCost">
+							Koszt:
+							<div id="C1">0</div>
+						</div>
+					</div>
+				</div>
+				<div class="cornerBlock">
+					<div id="startBlock">
+						Start
+					</div>
+					<div class="playersInCity">
+						<div id="playerInCity0">
+							<br>
+						</div>
+					</div>
+				</div>
+			</div>
+			</div>
+		</div>
+		<div class="players">
+			<div id="player3">
+				<div class="playerIn">
+					<div class="playerName">				
+					</div>
+					<div class="money">
+						Pieniądze: <br>
+						<div class="moneyAmmount">
+							<div class="moneyNumber">0</div>
+							<div>$</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="player4">
+				<div class="playerIn">
+					<div class="playerName">
+					</div>
+					<div class="money">
+						Pieniądze: <br>
+						<div class="moneyAmmount">
+							<div class="moneyNumber">0</div>
+							<div>$</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
